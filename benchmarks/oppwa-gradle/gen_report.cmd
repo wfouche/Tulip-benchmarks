0abceac39d3fe5759184f98a802315ae74f047c7
Werner Fouché <werner.fouche@gmail.com>
2025-03-14 17:35:24 +0200

    Fixed path

--- benchmarks/oppwa-gradle/gen_report.cmd
+++ benchmarks/oppwa-gradle/gen_report.cmd
@@ -1,4 +1,4 @@
 call .\gradlew -q run --args="--result oppwa_output.json"
 @echo off
 echo.
-..\..\platform\msys64\bin\w3m.exe -dump -cols 200 app/benchmark_report.html
+..\..\platform\msys64\bin\w3m.exe -dump -cols 200 app\oppwa_report.html
