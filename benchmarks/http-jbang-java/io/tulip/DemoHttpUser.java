d94e347f7441e5107c5aef85909b16e1fd3fe09c
Werner Fouché <werner.fouche@gmail.com>
2025-04-09 10:39:02 +0200

    Fixed Java http tests

--- benchmarks/http-jbang-java/io/tulip/DemoHttpUser.java
+++ benchmarks/http-jbang-java/io/tulip/DemoHttpUser.java
@@ -12,19 +12,42 @@ public class DemoHttpUser extends HttpUser {
     // Action 1: GET /posts/{id}
     public boolean action1() {
         int id = ThreadLocalRandom.current().nextInt(100)+1;
-        return http_GET("/posts/{id}", id);
+        return !http_GET("/posts/{id}", id).isEmpty();
     }
 
     // Action 2: GET /comments/{id}
     public boolean action2() {
         int id = ThreadLocalRandom.current().nextInt(500)+1;
-        return http_GET("/comments/{id}", id);
+        return !http_GET("/comments/{id}", id).isEmpty();
     }
 
     // Action 3: GET /todos/{id}
     public boolean action3() {
         int id = ThreadLocalRandom.current().nextInt(200)+1;
-        return http_GET("/todos/{id}", id);
+        return !http_GET("/todos/{id}", id).isEmpty();
+    }
+
+    // Action 4: POST /posts
+    public boolean action4() {
+        String body = new String("{\"title\": \"New Title\", \"body\": \"New Body\", \"userId\": 1}");
+        return !http_POST(body, "/posts").isEmpty();
+    }
+
+    // Action 5: PUT /posts/{id}
+    public boolean action5() {
+        String body = new String("{\"id\": 1, \"title\": \"New Title\", \"body\": \"New Body\", \"userId\": 1}");
+        return !http_PUT(body, "/posts/1").isEmpty();
+    }
+
+    // Action 6: PATCH /posts/{id}
+    public boolean action6() {
+        String body = new String("{\"title\": \"New Title\"}");
+        return !http_PATCH(body, "/posts/1").isEmpty();
+    }
+
+    // Action 7: DELETE /posts/{id}
+    public boolean action7() {
+        return !http_DELETE("/posts/1").isEmpty();
     }
 
 }    
