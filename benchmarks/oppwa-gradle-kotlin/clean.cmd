ccabf48967d0c6496577c85f86c11a14a9de06a7
Werner Fouché <werner.fouche@gmail.com>
2025-04-11 18:42:22 +0200

    Clean scripts for payments benchmark

--- benchmarks/oppwa-gradle-kotlin/clean.cmd
+++ benchmarks/oppwa-gradle-kotlin/clean.cmd
@@ -1,6 +1,7 @@
 rd/q/s .gradle
 rd/q/s .kotlin
 rd/q/s app\build
-del    app\oppwa_report*.html
+rd/q/s app\oppwa_report
+del    app\oppwa_report.html
 del    app\oppwa_config.adoc
 del    app\oppwa_output.json
---
+++ benchmarks/oppwa-gradle-kotlin/clean.sh
@@ -0,0 +1,7 @@
+rm -f -r .gradle
+rm -f -r .kotlin
+rm -f -r app\build
+rm -f -r app\oppwa_report
+rm -f app\oppwa_report.html
+rm -f app\oppwa_config.adoc
+rm -f app\oppwa_output.json
