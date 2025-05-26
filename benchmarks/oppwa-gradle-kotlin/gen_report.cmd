5fbecd95a2e6aeaf81ddfd8219e136dbc801f6c6
Werner Fouché <werner.fouche@gmail.com>
2025-05-26 15:16:09 +0200

    Use lynx instead of w3m

--- benchmarks/oppwa-gradle-kotlin/gen_report.cmd
+++ benchmarks/oppwa-gradle-kotlin/gen_report.cmd
@@ -1,4 +1,4 @@
 call .\gradlew -q run --args="--result oppwa_output.json --config=oppwa_config.json"
 @echo off
 echo.
-..\..\..\Tulip\platform\msys64\bin\w3m.exe -dump -cols 200 app\build\reports\tulip\oppwa_report.html
+lynx -dump -width 200 app\build\reports\tulip\oppwa_report.html
