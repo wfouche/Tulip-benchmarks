9c33ef2bf21cb0d71101df7a4d15f700304ffcd0
Werner Fouché <werner.fouche@gmail.com>
2025-07-29 08:48:07 +0200

    Remove stale comment

--- benchmarks/iso8583/TestServer.java
+++ benchmarks/iso8583/TestServer.java
@@ -12,29 +12,6 @@ import org.jpos.util.*;
 import org.jpos.iso.channel.*;
 import org.jpos.iso.packager.*;
 
-/**
- * Example of simple ISO-8583 server.
- * Listens and replies to ISO messages
- * <p>
- * Telnet to port 8000 and send an XML-formatted ISO-8583 message. 
- * (telnet localhost 8000)
- * You’ll get a response, with a result code "00" (field 39), e.g.:
- * <code>
- * (you type)
- * <isomsg>
- *    <field id="0" value="0800"/>
- *    <field id="3" value="333333"/>
- * </isomsg>
- * (and you should receive)
- * <isomsg direction="outgoing">
- *    <!-- org.jpos.iso.packager.XMLPackager -->
- *    <field id="0" value="0810"/>
- *    <field id="3" value="333333"/>
- *    <field id="39" value="00"/>
- * </isomsg>
- * </code>
- * (telnet ^] quit)
- */
 public class TestServer implements ISORequestListener {
 	public static final org.slf4j.Logger jLogger = 
 		org.slf4j.LoggerFactory.getLogger(TestServer.class);
