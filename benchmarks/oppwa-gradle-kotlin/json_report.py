78ea66c975f66dfdf9cee840899045ca64473635
Werner Fouché <werner.fouche@gmail.com>
2025-08-06 12:49:53 +0200

    Script to extract global avg_aps from JSON report file

---
+++ benchmarks/oppwa-gradle-kotlin/json_report.py
@@ -0,0 +1,8 @@
+import json
+from collections import OrderedDict
+
+filename = "app/build/reports/tulip/oppwa_report.json"
+fileObj = open(filename)
+jb = json.load(fileObj, object_pairs_hook=OrderedDict)
+
+print jb["benchmarks"]["Constant TPS"]["actions"]["summary"]["avg_aps"]
