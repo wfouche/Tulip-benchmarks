31adbf4f243c032a8c2a2f5e34706f134fc279d9
Werner Fouché <werner.fouche@gmail.com>
2025-08-10 10:16:42 +0200

    Use tulip-cli-dev for payments benchmark

--- benchmarks/http-jbang-tests/run_tests.cmd
+++ benchmarks/http-jbang-tests/run_tests.cmd
@@ -1,5 +1,6 @@
-set TULIP_PARAMS=10000.0 http://localhost:7070 2.1.8
+set TULIP_PARAMS=10000.0 http://localhost:7070 2.1.9-dev
 set TULIP_JAVA_OPTIONS=-server -Xms2g -Xmx2g -XX:+UseZGC -XX:+ZGenerational
+set TULI_CLI=tulip-cli-dev
 
 call jbang cache clear
 
@@ -7,7 +8,7 @@ call jbang --fresh run tulip-cli@wfouche
 
 if not exist "Java" (md Java)
 cd Java
-call jbang run tulip-cli@wfouche init Java %TULIP_PARAMS%
+call jbang run %TULI_CLI%@wfouche init Java %TULIP_PARAMS%
 call run_bench.cmd
 cd ..
 
@@ -19,18 +20,18 @@ REM cd ..
 
 if not exist "Kotlin" (md Kotlin)
 cd Kotlin
-call jbang run tulip-cli@wfouche init Kotlin %TULIP_PARAMS%
+call jbang run %TULI_CLI%@wfouche init Kotlin %TULIP_PARAMS%
 call run_bench.cmd
 cd ..
 
 if not exist "Scala" (md Scala)
 cd Scala
-call jbang run tulip-cli@wfouche init Scala %TULIP_PARAMS%
+call jbang run %TULI_CLI%@wfouche init Scala %TULIP_PARAMS%
 call run_bench.cmd
 cd ..
 
 if not exist "Jython" (md Jython)
 cd Jython
-call jbang run tulip-cli@wfouche init Jython %TULIP_PARAMS%
+call jbang run %TULI_CLI%@wfouche init Jython %TULIP_PARAMS%
 call run_bench.cmd
 cd ..
