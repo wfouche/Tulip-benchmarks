13b33c05c18e7d6481b8f12d170fc9778b6fe366
Werner Fouché <werner.fouche@gmail.com>
2025-08-11 06:44:49 +0200

    Updated json_report.py script

--- benchmarks/oppwa-gradle-kotlin/json_report.py
+++ benchmarks/oppwa-gradle-kotlin/json_report.py
@@ -5,4 +5,4 @@ filename = "app/build/reports/tulip/oppwa_report.json"
 fileObj = open(filename)
 jb = json.load(fileObj, object_pairs_hook=OrderedDict)
 
-print jb["benchmarks"]["Constant TPS"]["actions"]["summary"]["avg_aps"]
+print jb["benchmarks"]["Constant TPS"]["actions"]["summary"]["aps"]
