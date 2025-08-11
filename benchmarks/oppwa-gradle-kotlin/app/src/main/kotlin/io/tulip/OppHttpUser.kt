366de858d933cdea4eac4868f3ffb43f77c133fe
Werner Fouché <werner.fouche@gmail.com>
2025-08-11 11:49:18 +0200

    Renamed method getLogger() to logger()

--- benchmarks/oppwa-gradle-kotlin/app/src/main/kotlin/io/tulip/OppHttpUser.kt
+++ benchmarks/oppwa-gradle-kotlin/app/src/main/kotlin/io/tulip/OppHttpUser.kt
@@ -235,7 +235,7 @@ class OppHttpUser(userId: Int, threadId: Int) : HttpUser(userId, threadId) {
 
     // ----------------------------------------------------------------- //
 
-    override fun getLogger(): Logger {
+    override fun logger(): Logger {
         return logger
     }
 
