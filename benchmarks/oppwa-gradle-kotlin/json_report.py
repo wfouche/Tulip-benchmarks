3e0a3fce97df12fcb3811b8d1c38ce0dd86daca8
Werner Fouché <werner.fouche@gmail.com>
2025-08-13 19:44:22 +0200

    Display average aps rate and target aps rate

--- benchmarks/oppwa-gradle-kotlin/json_report.py
+++ benchmarks/oppwa-gradle-kotlin/json_report.py
@@ -5,4 +5,9 @@ filename = "app/build/reports/tulip/oppwa_report.json"
 fileObj = open(filename)
 jb = json.load(fileObj, object_pairs_hook=OrderedDict)
 
-print jb["benchmarks"]["Constant TPS"]["actions"]["summary"]["aps"]
+print "Constant TPS"
+print "  ", jb["benchmarks"]["Constant TPS"]["actions"]["summary"]["aps"]
+print "  ", jb["benchmarks"]["Constant TPS"]["actions"]["summary"]["aps_target_rate"]
+print "Rising TPS"
+print "  ", jb["benchmarks"]["Rising TPS"]["actions"]["summary"]["aps"]
+print "  ", jb["benchmarks"]["Rising TPS"]["actions"]["summary"]["aps_target_rate"]
