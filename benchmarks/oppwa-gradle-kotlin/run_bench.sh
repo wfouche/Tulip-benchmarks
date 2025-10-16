a30f21a31fea3c1ea755a193b9cef024704150c8
Werner Fouché <werner.fouche@gmail.com>
2025-10-16 10:35:35 +0200

    Java 21.0.8-tem (default)

--- benchmarks/oppwa-gradle-kotlin/run_bench.sh
+++ benchmarks/oppwa-gradle-kotlin/run_bench.sh
@@ -1,6 +1,6 @@
 rm -f app/app.log
 ./gradlew clean
-./gradlew run --args="--config=oppwa_config.json" --console=colored
+./gradlew run --args="--config=oppwa_config.json"
 echo ""
 #w3m -dump -cols 205 app/build/reports/tulip/oppwa_report.html
 lynx -dump -width 205 app/build/reports/tulip/oppwa_report.html
