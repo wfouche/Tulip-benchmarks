5f7611e7655724bc50fd61c855430eec7a77c6ec
Werner Fouché <werner.fouche@gmail.com>
2025-10-16 08:52:44 +0200

    Removed "echo."

--- benchmarks/oppwa-gradle-kotlin/run_bench.cmd
+++ benchmarks/oppwa-gradle-kotlin/run_bench.cmd
@@ -2,15 +2,13 @@
 
 set "LOG_FILE=.\app\app.log"
 
-:: Check if the file exists using the IF EXIST command.
+REM Check if the file exists using the IF EXIST command.
 if exist "%LOG_FILE%" (
     del %LOG_FILE%
     echo Success: "%LOG_FILE%" has been deleted.
 )
 
 call .\gradlew -q run --args="--config oppwa_config.json"
-
-echo.
 
 lynx -dump -width 205 .\app\build\reports\tulip\oppwa_report.html
 
