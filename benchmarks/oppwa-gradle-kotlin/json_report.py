9a18bf826af8de107b25af5a5547ac9baa22c75d
Werner Fouché <werner.fouche@gmail.com>
2025-11-09 20:32:37 +0200

    Updated json_report.py

--- benchmarks/oppwa-gradle-kotlin/json_report.py
+++ benchmarks/oppwa-gradle-kotlin/json_report.py
@@ -1,3 +1,10 @@
+# /// jbang
+# requires-jython = "2.7.4"
+# runtime-options = [
+#   "-Dpython.console.encoding=UTF-8"
+# ]
+# ///
+
 import json
 from collections import OrderedDict
 
@@ -8,6 +15,12 @@ jb = json.load(fileObj, object_pairs_hook=OrderedDict)
 print "Constant TPS"
 print "  ", jb["benchmarks"]["Constant TPS"]["actions"]["summary"]["aps"], "aps"
 print "  ", jb["benchmarks"]["Constant TPS"]["actions"]["summary"]["aps_target_rate"], "aps_target_rate"
+print "  ", jb["benchmarks"]["Constant TPS"]["actions"]["summary"]["avg_rt"], "avg_rt"
+print "  ", jb["benchmarks"]["Constant TPS"]["actions"]["summary"]["min_rt"], "min_rt"
+print "  ", jb["benchmarks"]["Constant TPS"]["actions"]["summary"]["max_rt"], "max_rt"
 print "Rising TPS"
 print "  ", jb["benchmarks"]["Rising TPS"]["actions"]["summary"]["aps"],"aps"
 print "  ", jb["benchmarks"]["Rising TPS"]["actions"]["summary"]["aps_target_rate"], "aps_target_rate"
+print "  ", jb["benchmarks"]["Rising TPS"]["actions"]["summary"]["avg_rt"], "avg_rt"
+print "  ", jb["benchmarks"]["Rising TPS"]["actions"]["summary"]["min_rt"], "min_rt"
+print "  ", jb["benchmarks"]["Rising TPS"]["actions"]["summary"]["max_rt"], "max_rt"
