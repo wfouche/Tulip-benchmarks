1145ab5944b9695fc88e9a6664f906af62b50aee
Werner Fouché <werner.fouche@gmail.com>
2025-11-20 23:13:05 +0200

    Script to product a bencher.dev BMF JSON file

---
+++ benchmarks/oppwa-gradle-kotlin/json_bencher.py
@@ -0,0 +1,28 @@
+# /// jbang
+# requires-jython = "2.7.4"
+# runtime-options = [
+#   "-Dpython.console.encoding=UTF-8"
+# ]
+# ///
+
+import json
+from collections import OrderedDict
+
+filename = "app/build/reports/tulip/oppwa_report.json"
+fileObj = open(filename)
+jb = json.load(fileObj, object_pairs_hook=OrderedDict)
+
+dict = {}
+dict["Constant TPS"] = "b900da0f-0f01-4658-a558-5e858f831813"
+print "{"
+print '  "%s": {'%(dict["Constant TPS"])
+print '    "latency": {'
+print '      "value": %.1f,'%(jb["benchmarks"]["Constant TPS"]["actions"]["summary"]["avg_rt"])
+print '      "lower_value": %.1f,'%(jb["benchmarks"]["Constant TPS"]["actions"]["summary"]["min_rt"])
+print '      "upper_value": %.1f'%(jb["benchmarks"]["Constant TPS"]["actions"]["summary"]["max_rt"])
+print '    },'
+print '    "throughput": {'
+print '      "value": %.1f'%(jb["benchmarks"]["Constant TPS"]["actions"]["summary"]["aps"])
+print '    }'
+print '  }'
+print "}"
