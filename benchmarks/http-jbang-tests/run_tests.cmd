9f6c8e2d577296a0df76e278c3ef3635cf060be0
Werner Fouché <werner.fouche@gmail.com>
2025-12-15 14:31:59 +0200

    Fixed TULIP_CLI name

--- benchmarks/http-jbang-tests/run_tests.cmd
+++ benchmarks/http-jbang-tests/run_tests.cmd
@@ -1,37 +1,44 @@
 set TULIP_PARAMS=10000.0 http://localhost:7070 2.1.14 HTTP
-set TULIP_JAVA_OPTIONS=-server -Xms2g -Xmx2g -XX:+UseZGC -XX:+ZGenerational
-set TULI_CLI=tulip-cli-dev@wfouche
+set TULIP_JAVA_OPTIONS=-Xms1g -Xmx2g -XX:+UseZGC -XX:+ZGenerational
+set TULIP_CLI=tulip-cli-dev@wfouche
 
 call jbang cache clear
 
 call jbang --fresh run %TULI_CLI%
 
+set TULIP_JAVA_OPTIONS=
+
+set JAVA_OPTS=
 if not exist "Java" (md Java)
 cd Java
-call jbang run %TULI_CLI% init Java %TULIP_PARAMS%
+call jbang run %TULIP_CLI% init Java %TULIP_PARAMS%
 call run_bench.cmd
 cd ..
 
+set JAVA_OPTS=
 if not exist "Groovy" (md Groovy)
 cd Groovy
-call jbang run %TULI_CLI% init Groovy %TULIP_PARAMS%
+call jbang run %TULIP_CLI% init Groovy %TULIP_PARAMS%
 call run_bench.cmd
 cd ..
 
+set JAVA_OPTS=
 if not exist "Kotlin" (md Kotlin)
 cd Kotlin
-call jbang run %TULI_CLI% init Kotlin %TULIP_PARAMS%
+call jbang run %TULIP_CLI% init Kotlin %TULIP_PARAMS%
 call run_bench.cmd
 cd ..
 
+set JAVA_OPTS=
 if not exist "Scala" (md Scala)
 cd Scala
-call jbang run %TULI_CLI% init Scala %TULIP_PARAMS%
+call jbang run %TULIP_CLI% init Scala %TULIP_PARAMS%
 call run_bench.cmd
 cd ..
 
+set JAVA_OPTS=
 if not exist "Jython" (md Jython)
 cd Jython
-call jbang run %TULI_CLI% init Jython %TULIP_PARAMS%
+call jbang run %TULIP_CLI% init Jython %TULIP_PARAMS%
 call run_bench.cmd
 cd ..
