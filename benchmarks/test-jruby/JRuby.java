832e370d3839aaee850bfc7fb5e820025a9903a2
Werner Fouché <werner.fouche@gmail.com>
2026-01-06 12:52:38 +0200

    Javalin updates

--- benchmarks/test-jruby/JRuby.java
+++ benchmarks/test-jruby/JRuby.java
@@ -1,6 +1,8 @@
 ///usr/bin/env jbang "$0" "$@" ; exit $?
 
+//DEPS org.slf4j:slf4j-simple:2.0.16
 //DEPS org.jruby:jruby-complete:10.0.2.0
+//DEPS io.javalin:javalin:6.4.0
 
 import org.jruby.Main;
 
