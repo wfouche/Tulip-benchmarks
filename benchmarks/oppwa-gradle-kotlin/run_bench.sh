d7b48bb62322d88ada97d24b160ab1d3541a90a0
Werner Fouché <werner.fouche@gmail.com>
2026-01-27 18:10:56 +0200

    Fixed run_bench.sh

--- benchmarks/oppwa-gradle-kotlin/run_bench.sh
+++ benchmarks/oppwa-gradle-kotlin/run_bench.sh
@@ -3,6 +3,6 @@ rm -f app/app.log
 ./gradlew run --args="--config=oppwa_config.json"
 echo ""
 #w3m -dump -cols 205 app/build/reports/tulip/oppwa_report.html
-lynx -dump -width 205 app/build/reports/tulip/oppwa_report.html
-echo ""
+#lynx -dump -width 205 app/build/reports/tulip/oppwa_report.html
+#echo ""
 ls -l app/app.log
