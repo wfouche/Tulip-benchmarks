defeb1e84e09fb9c816b3af82e2cda008445ca58
Werner Fouché <werner.fouche@gmail.com>
2026-01-28 12:57:47 +0200

    Build native app for Tulip benchmark

--- benchmarks/tulip-locust/tulip/build_native_app.sh
+++ benchmarks/tulip-locust/tulip/build_native_app.sh
@@ -1,3 +1,5 @@
+export SDKMAN_DIR="$HOME/.sdkman"
+[[ -s "$HOME/.sdkman/bin/sdkman-init.sh" ]] && source "$HOME/.sdkman/bin/sdkman-init.sh"
 rm -f ./App
 jbang cache clear
 sdk use java 25.0.2-graal
