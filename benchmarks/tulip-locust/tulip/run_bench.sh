e7962eceb322c73fd0d3acfa007037d6b4c601a8
Werner Fouché <werner.fouche@gmail.com>
2026-01-28 12:55:25 +0200

    Report is not auto generated by benchmark app

--- benchmarks/tulip-locust/tulip/run_bench.sh
+++ benchmarks/tulip-locust/tulip/run_bench.sh
@@ -5,6 +5,6 @@ export JBANG_APP_JAVA_OPTIONS="-Xmx2g -XX:+UseZGC"
 jbang run io/tulip/App.java
 echo ""
 #w3m -dump -cols 205 benchmark_report.html
-lynx -dump -width 205 benchmark_report.html
+#lynx -dump -width 205 benchmark_report.html
 #jbang run asciidoctorj@asciidoctor benchmark_config.adoc
 #jbang export fatjar io/tulip/App.java
