793153949e5b5a4e1fb9b3b6991cd47ee6c8f19f
Werner Fouché <werner.fouche@gmail.com>
2026-02-06 09:43:03 +0200

    Run http tests with Java 21

--- benchmarks/http-jbang-tests/run_tests.sh
+++ benchmarks/http-jbang-tests/run_tests.sh
@@ -16,8 +16,8 @@ jbang cache clear
 #
 # "-XX:+UseLargePages",
 
-export TULIP_PARAMS="10000.0 http://localhost:7070 2.2.1 HTTP_1_1 25"
-export TULIP_JAVA_OPTIONS="-Xms1g -Xmx2g -XX:+UseZGC"
+export TULIP_PARAMS="10000.0 http://localhost:7070 2.2.1 HTTP_1_1 21"
+export TULIP_JAVA_OPTIONS="-Xms1g -Xmx2g -XX:+UseZGC -XX:+ZGenerational"
 export TULIP_CLI="tulip-cli-dev@wfouche"
 #unset TULIP_JAVA_OPTIONS
 #export TULIP_JAVA_OPTIONS="-server -Xms2g -Xmx2g -XX:+UseZGC -XX:+ZGenerational -XX:+UseLargePages"
