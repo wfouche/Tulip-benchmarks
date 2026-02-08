9b6d9691be7e324f4de386e948b04238a56a5633
Werner Fouché <werner.fouche@gmail.com>
2026-02-08 20:19:46 +0200

    Removed ZGenerational

--- benchmarks/http-jbang-tests/run_tests.sh
+++ benchmarks/http-jbang-tests/run_tests.sh
@@ -17,7 +17,7 @@ jbang cache clear
 # "-XX:+UseLargePages",
 
 export TULIP_PARAMS="10000.0 http://localhost:7070 2.2.2 HTTP_1_1 21"
-export TULIP_JAVA_OPTIONS="-Xms1g -Xmx2g -XX:+UseZGC -XX:+ZGenerational"
+export TULIP_JAVA_OPTIONS="-Xms1g -Xmx2g -XX:+UseZGC"
 export TULIP_CLI="tulip-cli-dev@wfouche"
 #unset TULIP_JAVA_OPTIONS
 #export TULIP_JAVA_OPTIONS="-server -Xms2g -Xmx2g -XX:+UseZGC -XX:+ZGenerational -XX:+UseLargePages"
@@ -40,7 +40,6 @@ mkdir -p Groovy
 cd Groovy || exit
 echo ""
 jbang run $TULIP_CLI init Groovy $TULIP_PARAMS
-
 . ./run_bench.sh
 cd ..
 
