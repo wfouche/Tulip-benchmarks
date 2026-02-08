faec7b0ef1ff58970fe79263db265e30cba8da23
Werner Fouché <werner.fouche@gmail.com>
2026-02-08 14:25:01 +0200

    Use Java 21

--- benchmarks/oppwa-gradle-kotlin/app/build.gradle.kts
+++ benchmarks/oppwa-gradle-kotlin/app/build.gradle.kts
@@ -46,7 +46,7 @@ dependencies {
 // Apply a specific Java toolchain to ease working on different environments.
 java {
     toolchain {
-        languageVersion = JavaLanguageVersion.of(25)
+        languageVersion = JavaLanguageVersion.of(21)
         vendor = JvmVendorSpec.ADOPTIUM
     }
 }
